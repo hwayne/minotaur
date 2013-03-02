e50a4dd4bd0129d8db50369d535c3d3454a7cfd6
Hillel Wayne <hwayne@gmail.com>
2013-03-01 23:12:20 -0600

    Minotaur up

--- entity.py
+++ entity.py
@@ -6,19 +6,33 @@ class Entity:
         self.hist = [(x,y)]
         pass
 
+    def getpos(self):
+        return (self.xpos, self.ypos)
     #Are two entities in same place?
     def isOverlap(self, e):
         return (self.xpos == e.xpos) and (self.ypos == e.ypos)
 
     #Generic move.
     #Note you can delay with move(0).
-    def move(self, dist = 0, xmove = True ):
-        if xmove: self.xpos += dist
-        else: self.ypos += dist
-    #Play a whole turn.
-    def turn(self, dist = 0, xmove = True):
-        self.move(dist, xmove)
+    def moveX(self, dist = 1):
+        self.xpos += dist
+#Seperate moveY for simplicity.
+    def moveY(self, dist = 1):
+        self.ypos += dist
+
+#Check for collisions.
+#A collision is when an entity tries to move into
+#A square marked '_' or '|' (a wall)
+    def isCollision(self, char):
+        return (char == '_' or char == '|')
+
+    def updatehist(self):
         self.hist.append((self.xpos,self.ypos))
+    #Play a whole turn.
+    def turn(self):
+        self.moveX(1)
+        self.updatehist()
+
 
     #undo move. Requires history.
     def undo(self):
